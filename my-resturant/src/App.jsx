7ed01b5f5e77603b92772fb520effb839546fa2a
mohammad <mohahones@gmail.com>
2026-06-27 22:22:29 +0300

    addddddd

--- my-resturant/src/App.jsx
+++ my-resturant/src/App.jsx
@@ -81,7 +81,7 @@ export default function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route
--- my-resturant/src/main.jsx
+++ my-resturant/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const redirectPath = window.sessionStorage.getItem('gh-pages-redirect')
+
+if (redirectPath) {
+  window.sessionStorage.removeItem('gh-pages-redirect')
+  window.history.replaceState(null, '', redirectPath)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
