b2e6295aaf6ba9211571c0d9be7fea942f2d5ea4
mohammad <mohahones@gmail.com>
2026-06-27 22:04:55 +0300

    kk

--- my-resturant/src/App.jsx
+++ my-resturant/src/App.jsx
@@ -81,7 +81,7 @@ export default function App() {
   }
 
   return (
-    <BrowserRouter basename="/aklatna">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route
