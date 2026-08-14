00b32fb4d9a1b697b6510fc947213fcfef71796b
mohammad <mohahones@gmail.com>
2026-08-14 13:13:55 +0300

    add

--- my-resturant/src/main.jsx
+++ my-resturant/src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
+    navigator.serviceWorker.register('sw.js').then(reg => {
       console.log('Service worker registered:', reg.scope);
     }).catch(err => console.warn('SW register failed:', err));
   });
